ad82e53b3a2b984d9f2c9b579f62c2d0f5f957b6
nicoleoprice <82247995+nicoleoprice@users.noreply.github.com>
2022-06-01 19:13:04 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let num = 40;
+let mouseXArray = [];
+let mouseYArray = [];
+
+function setup() {
+  createCanvas(400, 400);
+
+  
+  for(let i = 0; i < num; i++) {
+    mouseXArray.push(i);
+    mouseYArray.push(i);
+  }
+}
+
+function draw() {
+  background(0);
+  
+  let j = frameCount % num;
+  mouseXArray[j] = mouseX;
+  mouseYArray[j] = mouseY;
+  
+  for (let i = 0; i < num; i++) {
+    let k = (j + 1 + i) % num; 
+    rect(mouseXArray[k], mouseYArray[k], i, i, 10);
+  }
+        noStroke();
+  r = random(0,255);
+  g = random(0,255);
+  b = random(0,255);
+  fill(r, g, b);
+}
