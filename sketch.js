641863c570d837ffc8b5df6f35d3c967c5eb2202
nicoleoprice <82247995+nicoleoprice@users.noreply.github.com>
2022-06-01 23:58:47 -0400

    shape change

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function draw() {
   
   for (let i = 0; i < num; i++) {
     let k = (j + 1 + i) % num; 
-    ellipse(mouseXArray[k], mouseYArray[k], i, i, 10);
+    rect(mouseXArray[k], mouseYArray[k], i, i, 10);
   }
         noStroke();
   r = random(0,255);
